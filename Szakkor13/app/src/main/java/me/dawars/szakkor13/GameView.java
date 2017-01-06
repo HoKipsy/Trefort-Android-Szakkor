d168dad867420d03882baf299dab8a6a046dbcce
dawars <dawars00@gmail.com>
2017-01-06 20:24:26 +0100

    Added game loop Falling circles

--- Szakkor13/app/src/main/java/me/dawars/szakkor13/GameView.java
+++ Szakkor13/app/src/main/java/me/dawars/szakkor13/GameView.java
@@ -1,17 +1,10 @@
 package me.dawars.szakkor13;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by dawars on 1/5/17.
@@ -20,21 +13,23 @@ import java.util.List;
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private static final float RADIUS = 100;
     private final SurfaceHolder surfaceHolder;
-    private final Paint paint = new Paint();
+
+    private final GameThread gameThread;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        paint.setColor(Color.WHITE);
-        paint.setStrokeWidth(5);
 
         surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
+
+        gameThread = new GameThread(surfaceHolder);
 
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        tick();
+        gameThread.start(); // nem run!!!
+
     }
 
     @Override
@@ -47,42 +42,10 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     }
 
-    List<PointF> objects = new LinkedList<>();
-
-    private void tick() {
-
-//        update(); // objectek frissitese
-
-        // lockoljuk es lekerjuk a canvas-t, hogy mas ne ferjen hozza, amig hasznaljuk
-        Canvas canvas = surfaceHolder.lockCanvas();
-
-        if (canvas != null) {
-            canvas.drawColor(Color.argb(255, 255, 0, 255));
-            render(canvas); // rajzolunk
-            // befejezzuk a rajzolast es odaadjuk a rendszernek megjelenitesre:
-            surfaceHolder.unlockCanvasAndPost(canvas);
-        }
-    }
-/*  Needs game loop
-    private void update() {
-        for (PointF point : objects) { // ne hasznaljunk foreach-et ha torlunk a tombbol!
-            point.set(point.x, point.y += 10);
-        }
-    }*/
-
-    private void render(Canvas canvas) {
-        for (PointF point : objects) {
-            canvas.drawCircle(point.x, point.y, RADIUS, paint);
-        }
-    }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // other gestures, multitouch?
 
-        objects.add(new PointF(event.getX(), event.getY()));
-        tick();
+        gameThread.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
-
 }
